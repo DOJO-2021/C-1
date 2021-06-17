d0e46f31ce0f07d0af68cdb44fcac62bee3f340e
niikawa@seplus2016.onmicrosoft.com <takafumi0110>
2021-06-17 10:36:04 +0900

    更新-新川

--- TeraChannel/src/servlet/ManagerLogoutServlet.java
+++ TeraChannel/src/servlet/ManagerLogoutServlet.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpSession;
 /**
  * Servlet implementation class LogoutServlet
  */
-@WebServlet("/LogoutServlet")
+@WebServlet("/ManagerLogoutServlet")
 public class ManagerLogoutServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
